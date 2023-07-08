7f3a8acfad8d1197d95b84a8d8d6e116f8179a13
w w <lupsetudor@gmail.com>
2023-07-09 02:44:33 +0300

    comitez

--- javascript/scriptIndex.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function () {
-    var scrollingElement = document.getElementById('scroll');
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    scrollingElement.style.top = scrollTop + 'px';
-})
